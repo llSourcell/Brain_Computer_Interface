874baed8dfe1302b3e6194ecc8259ee7b24cb7ae
Siraj <sirajravel@gmail.com>
2017-09-20 17:13:43 +0200

    Update eeg.py

--- eeg.py
+++ eeg.py
@@ -2,10 +2,6 @@
 #  This class is responsible for:
 #    - reading in the raw packet data from the /dev/eeg/encrypted device
 #    - decrypting the signal (two 16-byte packets, ECB mode AES)
-#    - queuing the decoded packets for buffer pull requests
-#    - forwarding the packets to registered subscribers
-#    - passing the packets to the EmotivDevice for updating
-#
 
 import string
 import threading
